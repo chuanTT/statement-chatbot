3e06983b59b125259715decab5da0528b02ae323
chuantt <chuan123789@gmail.com>
2024-02-27 10:07:39 +0700

    update .env

--- src/data-source.ts
+++ src/data-source.ts
@@ -1,17 +1,19 @@
-import "reflect-metadata"
-import { DataSource } from "typeorm"
+import "reflect-metadata";
+import { DataSource } from "typeorm";
+import { config } from "dotenv";
+config()
 
 export const AppDataSource = new DataSource({
-    type: "mysql",
-    host: "127.0.0.1",
-    port: 3306,
-    username: "root",
-    password: "",
-    database: "chuandinh",
-    connectorPackage:"mysql2",
-    synchronize: true,
-    logging: false,
-    entities: [__dirname + "/entity/*.ts"],
-    migrations: [__dirname + "/migration/*.ts"],
-    subscribers: [],
-})
+  type: "mysql",
+  host: process.env.HOST,
+  port: 3306,
+  username: process.env.USER_NAME,
+  password: process.env.PASSWORD,
+  database: process.env.DATABASE,
+  connectorPackage: "mysql2",
+  synchronize: true,
+  logging: false,
+  entities: [__dirname + "/entity/*.ts"],
+  migrations: [__dirname + "/migration/*.ts"],
+  subscribers: [],
+});
