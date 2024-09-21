f729cb9fe128a1323e37132c42b47de1ed749391
chuanTT <chuan123789@gmail.com>
2024-09-21 12:32:52 +0700

    update

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,41 @@
-import { AppDataSource } from "./data-source"
-import { BankTransaction } from "./entity/BankTransaction"
-import app from "./server"
-const PORT = 3001
+import { readFileSync } from "fs";
+import { AppDataSource } from "./data-source";
+import { BankTransaction } from "./entity/BankTransaction";
+import app from "./server";
+import { join } from "path";
+import * as TelegramBot from "node-telegram-bot-api";
+const PORT = 3001;
 
-AppDataSource.initialize().then(async () => {
-    const bankTransactonRepository = AppDataSource.manager.getRepository(BankTransaction)
+AppDataSource.initialize()
+  .then(async () => {
+    // replace the value below with the Telegram token you receive from @BotFather
+    const token = "7802074927:AAH59keNnZAJaBLj7XOyhRxb0tyDhBGXdPs";
 
-    const bankTransaction = new BankTransaction();
-    //   bankTransaction.accountNumber = "";
-    //   bankTransaction.people = people || 1;
-    //   bankTransaction.isPaid = isPaid ? 1 : 0;
-    //   bankTransaction.user = user;
-    bankTransactonRepository.save(bankTransaction)
-    app.listen(PORT, () => console.log(`server lister port:${PORT} `))
-}).catch(error => console.log(error))
+    // Create a bot that uses 'polling' to fetch new updates
+    const bot = new TelegramBot(token, { polling: true });
+
+    // Matches "/echo [whatever]"
+    bot.onText(/\/echo (.+)/, (msg, match) => {
+      // 'msg' is the received Message from Telegram
+      // 'match' is the result of executing the regexp above on the text content
+      // of the message
+
+      const chatId = msg.chat.id;
+      const resp = match[1]; // the captured "whatever"
+
+      // send back the matched "whatever" to the chat
+      bot.sendMessage(chatId, resp);
+    });
+
+    // Listen for any kind of message. There are different kinds of
+    // messages.
+    bot.on("message", (msg) => {
+      const chatId = msg.chat.id;
+
+      // // send a message to the chat acknowledging receipt of their message
+      bot.sendMessage(chatId, "Received your message");
+    });
+
+    app.listen(PORT, () => console.log(`server lister port:${PORT} `));
+  })
+  .catch((error) => console.log(error));
--- src/migration/1726850226112-init.ts.ts
+++ src/migration/1726850226112-init.ts.ts
@@ -1,9 +1,33 @@
-import { readFileSync } from "fs";
-import { MigrationInterface } from "typeorm";
+import { readdirSync, readFileSync } from "fs";
+import { MigrationInterface, QueryRunner } from "typeorm";
+import { AppDataSource } from "../data-source";
+import { BankTransaction } from "../entity/BankTransaction";
+import { join } from "path";
+
+const sleep = async (ms: number) => {
+  return await new Promise((resolve) => setTimeout(resolve, ms));
+};
 
 export class InitTs1726850226112 implements MigrationInterface {
   public async up(): Promise<void> {
-    const data = readFileSync("./src/Store/BIDV/01.09-12.09.2024.json");
+    const bankTransaction = AppDataSource.getRepository(BankTransaction)
+    let path = __dirname.split("\\");
+    path = path?.slice(0, path?.length - 1);
+    const pathStore = join(...path, "Store");
+    const listDir = readdirSync(pathStore);
+    for (const dir of listDir) {
+      const pathDir = join(pathStore, dir);
+      const listFile = readdirSync(join(pathStore, dir));
+      for (const file of listFile) {
+        const dataFile = JSON.parse(
+          readFileSync(join(pathDir, file), {
+            encoding: "utf8",
+          })
+        );
+        await bankTransaction.insert(dataFile)
+        sleep(500);
+      }
+    }
   }
 
   public async down(): Promise<void> {}
